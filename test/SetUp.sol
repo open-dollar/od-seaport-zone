0ca14ee7474f8844bcbbbfb02098d862e9b847af
MrDeadCe11 <mister.deadce11@gmail.com>
2024-05-21 23:00:22 -0500

    more set up

--- test/SetUp.sol
+++ test/SetUp.sol
@@ -6,7 +6,18 @@ import {
 } from 'seaport-types/src/interfaces/AbridgedTokenInterfaces.sol';
 
 import {ERC165} from '@openzeppelin/contracts/utils/introspection/ERC165.sol';
-import {ReceivedItem, Schema, SpentItem, ZoneParameters} from 'seaport-types/src/lib/ConsiderationStructs.sol';
+import {
+  ReceivedItem,
+  Schema,
+  SpentItem,
+  ZoneParameters,
+  AdditionalRecipient,
+  Fulfillment,
+  FulfillmentComponent,
+  Order,
+  OrderComponents,
+  OrderParameters
+} from 'seaport-types/src/lib/ConsiderationStructs.sol';
 
 import {ItemType} from 'seaport-types/src/lib/ConsiderationEnums.sol';
 
@@ -17,16 +28,28 @@ import {ZoneInterface} from 'seaport-types/src/interfaces/ZoneInterface.sol';
 import {Test} from 'forge-std/Test.sol';
 import {ODNFVZone} from '../src/contracts/ODNFVZone.sol';
 import {ODNFVZoneInterface} from '../src/interfaces/ODNFVZoneInterface.sol';
-
+import {ODNFVControllerInterface} from '../src/interfaces/ODNFVZoneControllerInterface.sol';
+import {ODNFVController} from '../src/contracts/ODNFVZoneController.sol';
 import {Seaport as CoreSeaport} from 'seaport-core/Seaport.sol';
+
 import 'forge-std/console2.sol';
 
 contract SetUp is Test {
+  address deployer = address(0xdeadce11);
   address payable public seaportMainnetAddress = payable(address(0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC)); //seaport 1.5 on arb mainnet
-  CoreSeaport public seaport = CoreSeaport(seaportMainnetAddress);
+  CoreSeaport public seaport;
+  ODNFVZoneInterface public zone;
+  ODNFVControllerInterface public zoneController;
 
   function setUp() public {
     //create arb mainnet fork;
-    vm.createSelectFork(vm.envString('[ARB_MAINNET_RPC'));
+    vm.createSelectFork(vm.envString('ARB_MAINNET_RPC'));
+    vm.deal(deployer, 1000 ether);
+    vm.startPrank(deployer);
+    seaport = CoreSeaport(seaportMainnetAddress);
+    zoneController = new ODNFVController(deployer);
+    zone = zoneController.createZone(keccak256(abi.encode('salt')));
   }
+
+  function createOffer() public {}
 }
