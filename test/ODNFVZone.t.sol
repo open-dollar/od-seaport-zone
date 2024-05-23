f321f4ebdcac1f328bb5fee8c02674c5df3c0421
MrDeadCe11 <mister.deadce11@gmail.com>
2024-05-23 15:02:57 -0500

    uhm yeah, apperently I could have just imported baseOrderTest this whole time.

--- test/ODNFVZone.t.sol
+++ test/ODNFVZone.t.sol
@@ -2,27 +2,71 @@
 pragma solidity ^0.8.24;
 
 import {
-  ERC20Interface, ERC721Interface, ERC1155Interface
-} from 'seaport-types/src/interfaces/AbridgedTokenInterfaces.sol';
+    AdvancedOrder,
+    ConsiderationItem,
+    CriteriaResolver,
+    Fulfillment,
+    FulfillmentComponent,
+    ItemType,
+    OfferItem,
+    Order,
+    OrderComponents,
+    OrderType
+} from "seaport-types/src/lib/ConsiderationStructs.sol";
 
-import {ERC165} from '@openzeppelin/contracts/utils/introspection/ERC165.sol';
-import {ReceivedItem, Schema, SpentItem, ZoneParameters} from 'seaport-types/src/lib/ConsiderationStructs.sol';
+import { UnavailableReason } from "seaport-sol/src/SpaceEnums.sol";
 
-import {ItemType, OrderType, Side} from 'seaport-sol/src/SeaportEnums.sol';
+import {
+    ConsiderationInterface
+} from "seaport-types/src/interfaces/ConsiderationInterface.sol";
 
-import {ContractOffererInterface} from 'seaport-types/src/interfaces/ContractOffererInterface.sol';
+import {
+    ConsiderationItemLib,
+    FulfillmentComponentLib,
+    FulfillmentLib,
+    OfferItemLib,
+    OrderComponentsLib,
+    OrderLib,
+    SeaportArrays
+} from "seaport-sol/src/lib/SeaportStructLib.sol";
 
-import {ZoneInterface} from 'seaport-types/src/interfaces/ZoneInterface.sol';
+import {
+    TestTransferValidationZoneOfferer
+} from "seaport/contracts/test/TestTransferValidationZoneOfferer.sol";
 
-import {Test} from 'forge-std/Test.sol';
+import {
+    FulfillAvailableHelper
+} from "seaport-sol/src/fulfillments/available/FulfillAvailableHelper.sol";
+
+import {
+    MatchFulfillmentHelper
+} from "seaport-sol/src/fulfillments/match/MatchFulfillmentHelper.sol";
+
 import {ODNFVZone} from '../src/contracts/ODNFVZone.sol';
 import {ODNFVZoneInterface} from '../src/interfaces/ODNFVZoneInterface.sol';
 import {SetUp} from './SetUp.sol';
 import 'forge-std/console2.sol';
 
 contract ODNFVZoneTest is SetUp {
-  function testFork() public {
-    (string memory version, bytes32 domainSeparator, address conduitController) = seaport.information();
-    console2.log(conduitController);
-  }
+    using FulfillmentLib for Fulfillment;
+    using FulfillmentComponentLib for FulfillmentComponent;
+    using FulfillmentComponentLib for FulfillmentComponent[];
+    using OfferItemLib for OfferItem;
+    using OfferItemLib for OfferItem[];
+    using ConsiderationItemLib for ConsiderationItem;
+    using ConsiderationItemLib for ConsiderationItem[];
+    using OrderComponentsLib for OrderComponents;
+    using OrderLib for Order;
+    using OrderLib for Order[];
+
+    MatchFulfillmentHelper matchFulfillmentHelper;
+    FulfillAvailableHelper fulfillAvailableFulfillmentHelper;
+    ODNFVZone zone;
+
+    function setUp()public virtual override {
+      super.setUp();
+       
+    }
+
+   
 }
