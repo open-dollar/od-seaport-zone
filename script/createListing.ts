45f400733c050e7309fa2c686511180f7d7ff816
MrDeadCe11 <84342118+MrDeadCe11@users.noreply.github.com>
2024-06-17 17:08:13 -0500

    Create listing (#34)
    * deploy script up
    
    * deploy script up
    
    * deploy with cast wallet or .emv
    
    * linted
    
    * creating listing script
    
    * working on using the seaport sdk
    
    * got scripts compiling and running, erroring.
    
    * things seem to be working somewhat, still erroring, but not battling types and dependencies any more
    
    * added missing files
    
    * added missing files
    
    * an ENS name used for a contract target must be correctly configured
    
    * order being created now
    
    * constants.ts now fetches addresses from .json files or from .env if there is no json
    
    * successfully created a listing with the deployed zone
    
    * code formatting and cleanup
    
    * created web3ness class
    
    * changed name of web3ness
    
    * removed fullill file
    
    * updating node version
    
    * updating node version
    
    * linted
    
    * fixing test.yml
    
    * linted again
    
    * Untrack types, cleanup package.json
    
    ---------
    
    Co-authored-by: Patrick Gallagher <blockchainbuddha@gmail.com>

---
+++ script/createListing.ts
@@ -0,0 +1,103 @@
+import { BigNumberish, BytesLike, ethers } from "ethers";
+import {
+  Web3Environment
+} from "./utils/constants";
+import { ItemType } from "@opensea/seaport-js/src/constants";
+import { CreateOrderInput } from "@opensea/seaport-js/lib/types";
+
+
+
+const createSIP15ZoneListing = async (chain: string) => {
+  const web3Env = new Web3Environment(chain);
+  const vault721Adapter = web3Env.vault721Adapter;
+  const encodeSubstandard5Helper = web3Env.encodeSubstandard5Helper;
+  const vault721AdapterAddress = web3Env.vault721AdapterAddress;
+  const vault721Address = web3Env.vault721Address;
+  const provider = web3Env.provider;
+  const sip15ZoneAddress = web3Env.sip15ZoneAddress;
+  const seaport = web3Env.seaport;
+  const wallet = web3Env.wallet;
+
+  /** @TODO  Fill in the token address and token ID of the NFT you want to sell, as well as the price */
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  let considerationTokenAddress: string =
+    "0x8c12A21C8D62d794f78E02aE9e377Abee4750E87";
+  let vaultId: string = "120";
+  let listingAmount: string = ethers.parseEther("1").toString();
+
+
+
+  const _comparisonEnums: BigNumberish[] = [4, 5] as BigNumberish[];
+  const _traitKeys: BytesLike[] = [
+    ethers.keccak256(ethers.toUtf8Bytes("DEBT")),
+    ethers.keccak256(ethers.toUtf8Bytes("COLLATERAL")),
+  ];
+
+  const _traitValues: BytesLike[] = await vault721Adapter
+    .getTraitValues(ethers.toBigInt(vaultId), _traitKeys)
+    .then((array) =>
+      array.map((e: BytesLike) => {
+        return e;
+      })
+    );
+
+  //create encoded substandard 5 data with helper
+  const extraData = await encodeSubstandard5Helper!.encodeSubstandard5(
+    _comparisonEnums,
+    vault721Address,
+    vault721AdapterAddress,
+    vaultId,
+    _traitValues,
+    _traitKeys
+  );
+
+  // get zone hash by hashing extraData
+  const zoneHash = ethers.keccak256(extraData);
+  const timeStamp = (await provider.getBlock("latest"))!.timestamp;
+
+  const createOrderInput: CreateOrderInput = {
+    offer: [
+      {
+        itemType: ItemType.ERC721,
+        token: vault721Address,
+        identifier: vaultId,
+      },
+    ],
+    consideration: [
+      {
+        token: considerationTokenAddress,
+        amount: ethers.parseEther(listingAmount).toString(),
+      },
+    ],
+    startTime: timeStamp,
+    endTime: timeStamp,
+    zoneHash: zoneHash,
+    zone: sip15ZoneAddress,
+    restrictedByZone: true,
+  };
+
+  try {
+    const { executeAllActions } = await seaport.createOrder(
+      createOrderInput,
+      wallet.address
+    );
+    const order = await executeAllActions();
+    console.log(
+      "Successfully created a listing with orderHash:",
+      order.parameters
+    );
+  } catch (error) {
+    console.error("Error in createListing:", error);
+  }
+};
+
+// Check if the module is the main entry point
+if (require.main === module) {
+  // If yes, run the createOffer function
+  createSIP15ZoneListing("sepolia").catch((error) => {
+    console.error("Error in createListing:", error);
+  });
+}
+
+export default createSIP15ZoneListing;
---
+++ script/utils/constants.ts
@@ -0,0 +1,230 @@
+require("dotenv").config();
+import fs from "fs";
+import path from "path";
+import { ethers, Wallet, Provider } from "ethers";
+import { Seaport } from "@opensea/seaport-js";
+import {
+  Vault721Adapter,
+  EncodeSubstandard5ForEthers,
+} from "../../types/ethers-contracts/index";
+
+const baseSepoliaPath = path.join(
+  "node_modules/@opendollar/contracts/script/SepoliaContracts.s.sol"
+);
+const baseMainnetPath = path.join(
+  "node_modules/@opendollar/contracts/script/MainnetContracts.s.sol"
+);
+
+const baseSepoliaDeploymentsPath = path.join(
+  "broadcast/DeploySIP15Zone.s.sol/421614/run-latest.json"
+);
+
+
+/** @note uncomment next line when mainnet deployment has been done */
+// const baseMainnetDeploymentsPath = path.join('broadcast/DeploySIP15Zone.s.sol/42161/run-latest.json');
+
+/** @note delete this next line when mainnet deployment is done */
+const baseMainnetDeploymentsPath = path.join(
+  "broadcast/DeploySIP15Zone.s.sol/31337/run-latest.json"
+);
+/** @note do the things said above */
+
+const sepoliaContractsString: string = fs.readFileSync(
+  baseSepoliaPath,
+  "utf-8"
+);
+const mainnetContractsString: string = fs.readFileSync(
+  baseMainnetPath,
+  "utf-8"
+);
+const sepoliaDeployments = JSON.parse(
+  fs.readFileSync(baseSepoliaDeploymentsPath, "utf-8")
+);
+const mainnetDeployments = JSON.parse(
+  fs.readFileSync(baseMainnetDeploymentsPath, "utf-8")
+);
+
+const sepoliaContracts = stringToObject(sepoliaContractsString);
+const mainnetContracts = stringToObject(mainnetContractsString);
+
+type ParsedObjectType = {
+  [key: string]: string;
+};
+
+function stringToObject(str: string): ParsedObjectType {
+  const parts = str.split(";");
+  const trimmedObject: ParsedObjectType = {};
+  parts
+    .filter((element, i) => {
+      return element.includes("=");
+    })
+    .forEach((e, i) => {
+      const searchString = "address public";
+      const thing = e.indexOf(searchString);
+      const [key, value] = e
+        .trim()
+        .slice(thing + 12)
+        .split("=");
+      const trimmedKey = key.trim().replace(/^address\s+/, "");
+      const trimmedValue = value.trim().replace(/;$/, "");
+      trimmedObject[trimmedKey] = trimmedValue;
+    });
+  return trimmedObject;
+}
+
+export const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY;
+export const ARB_SEPOLIA_PK = process.env.ARB_SEPOLIA_PK;
+export const ARB_MAINNET_PK = process.env.ARB_MAINNET_PK;
+export const ARB_SEPOLIA_RPC = process.env.ARB_SEPOLIA_RPC;
+export const ARB_MAINNET_RPC = process.env.ARB_MAINNET_RPC;
+
+
+//   export const SIP15_ZONE_MAINNET_ADDRESS = checkMainnetAddress(mainnetDeployments, 0)
+export const SIP15_ZONE_SEPOLIA_ADDRESS = checkSepoliaAddress(
+  sepoliaDeployments,
+  0
+);
+
+export const VAULT721_SEPOLIA_ADAPTER_ADDRESS = checkSepoliaAddress(
+  sepoliaDeployments,
+  1
+);
+
+//   export const VAULT721_MAINNET_ADAPTER_ADDRESS = checkMainnetAddress(mainnetDeployments, 1)
+export const VAULT721_SEPOLIA_ADDRESS = sepoliaContracts.Vault721_Address;
+export const VAULT721_MAINNET_ADDRESS = mainnetContracts.Vault721_Address;
+export const VAULT721_ANVIL_ADDRESS = process.env.VAULT721_ANVIL_ADDRESS;
+
+//   export const ENCODING_HELPER_MAINNET = checkMainnetAddress(mainnetDeployments, 2)
+export const ENCODING_HELPER_SEPOLIA = checkSepoliaAddress(
+  sepoliaDeployments,
+  2
+);
+
+export const Vault721AdapterABI = require("../../out/Vault721Adapter.sol/Vault721Adapter.json");
+export const EncodeSubstandard5ForEthersABI = require("../../out/EncodeSubstandard5ForEthers.sol/EncodeSubstandard5ForEthers.json");
+
+export class Web3Environment {
+  provider: Provider;
+  wallet: Wallet;
+  seaport: Seaport;
+  encodeSubstandard5Helper: EncodeSubstandard5ForEthers | undefined;
+  vault721AdapterAddress: string;
+  vault721Address: string;
+  sip15ZoneAddress: string;
+  vault721Adapter:Vault721Adapter;
+
+  constructor(chain:string){
+    if (chain == "sepolia") {
+      this.provider = new ethers.JsonRpcProvider(ARB_SEPOLIA_RPC);
+      this.wallet = new ethers.Wallet(ARB_SEPOLIA_PK as string, this.provider);
+      this.seaport = new Seaport(this.wallet);
+  
+      if (
+        VAULT721_SEPOLIA_ADAPTER_ADDRESS &&
+        SIP15_ZONE_SEPOLIA_ADDRESS &&
+        VAULT721_SEPOLIA_ADDRESS
+      ) {
+        this.vault721AdapterAddress = VAULT721_SEPOLIA_ADAPTER_ADDRESS;
+        this.vault721Address = VAULT721_SEPOLIA_ADDRESS;
+        this.sip15ZoneAddress = SIP15_ZONE_SEPOLIA_ADDRESS;
+      } else {
+        throw new Error("VAULT721_SEPOLIA_ADAPTER_ADDRESS undefined");
+      }
+      // if no helper exists deploy helper
+      if (!ENCODING_HELPER_SEPOLIA) {
+        this.deployEncodingHelper();
+      } else {
+        this.encodeSubstandard5Helper = new ethers.Contract(
+          ENCODING_HELPER_SEPOLIA,
+          EncodeSubstandard5ForEthersABI.abi,
+          this.wallet
+        ) as unknown as EncodeSubstandard5ForEthers;
+      }
+      // } else if (chain == 'mainnet'){
+      //   provider = new ethers.JsonRpcProvider(ARB_MAINNET_RPC);
+      //   wallet = new ethers.Wallet(ARB_MAINNET_PK as string, provider);
+      //   seaport = new Seaport(wallet);
+  
+      //   if (VAULT721_MAINNET_ADAPTER_ADDRESS && VAULT721_MAINNET_ADDRESS) {
+      //     vault721AdapterAddress = VAULT721_MAINNET_ADAPTER_ADDRESS;
+      //     vault721Address = VAULT721_MAINNET_ADDRESS;
+      //   } else {
+      //     throw new Error("VAULT721_MAINNET_ADAPTER_ADDRESS undefined");
+      //   }
+  
+      //   // if no helper exists deploy helper
+        // this.deployEncodingHelper();
+      //   } else {
+      //     encodeSubstandard5Helper = new ethers.Contract(
+      //       ENCODING_HELPER_MAINNET,
+      //       EncodeSubstandard5ForEthersABI.abi,
+      //       wallet
+      //     ) as unknown as EncodeSubstandard5ForEthers;
+      //   }
+    } else {
+      throw new Error("unsupported chain");
+    }
+
+    this.vault721Adapter = new ethers.Contract(
+      this.vault721AdapterAddress,
+      Vault721AdapterABI.abi,
+      this.wallet
+    ) as unknown as Vault721Adapter;
+  
+  }
+
+  async deployEncodingHelper(){
+    const encodeSubstandard5Factory = new ethers.ContractFactory(
+      EncodeSubstandard5ForEthersABI.abi,
+      EncodeSubstandard5ForEthersABI.bytecode,
+      this.wallet
+    );
+    this.encodeSubstandard5Helper =
+      (await encodeSubstandard5Factory.deploy()) as EncodeSubstandard5ForEthers;
+  }
+
+}
+
+function checkMainnetAddress(
+  deployment: any,
+  index: number
+): string | undefined {
+  return (() => {
+    try {
+      return deployment.receipts[index].contractAddress;
+    } catch (error) {
+      if (index == 0 && process.env.SIP15_ZONE_MAINNET_ADDRESS) {
+        return process.env.SIP15_ZONE_MAINNET_ADDRESS;
+      } else if (index == 1 && process.env.VAULT721_MAINNET_ADAPTER_ADDRESS) {
+        return process.env.VAULT721_MAINNET_ADAPTER_ADDRESS;
+      } else if (index == 2 && process.env.ENCODING_HELPER_MAINNET) {
+        return process.env.ENCODING_HELPER_MAINNET;
+      } else {
+        console.error(error);
+      }
+    }
+  })();
+}
+
+function checkSepoliaAddress(
+  deployment: any,
+  index: number
+): string | undefined {
+  return (() => {
+    try {
+      return deployment.receipts[index].contractAddress;
+    } catch (error) {
+      if (index == 0 && process.env.SIP15_ZONE_SEPOLIA_ADDRESS) {
+        return process.env.SIP15_ZONE_SEPOLIA_ADDRESS;
+      } else if (index == 1 && process.env.VAULT721_SEPOLIA_ADAPTER_ADDRESS) {
+        return process.env.VAULT721_SEPOLIA_ADAPTER_ADDRESS;
+      } else if (index == 2 && process.env.ENCODING_HELPER_SEPOLIA) {
+        return process.env.ENCODING_HELPER_SEPOLIA;
+      } else {
+        console.error(error);
+        throw new Error("addresses cannot be gotten");
+      }
+    }
+  })();
+}
